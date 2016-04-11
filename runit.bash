d618237c63fa6399cd5c4068969dfe5a8a527867
Gina White <ginabythebay@gmail.com>
2016-04-11 14:36:22 -0700

    Let docker work on mac where user is already ok

--- runit.bash
+++ runit.bash
@@ -2,6 +2,9 @@
 
 set -e
 
+platform="$(uname)"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IMG=cheatsheets
 
 docker build -t $IMG .
@@ -11,5 +14,10 @@ mkdir build
 cp *.tex build/
 cp incontainer.bash build/
 
-docker run  -v /home/gina/Source/cheat-sheets/build:/home/build -u $(id -u) $IMG
+user_option="-u $(id -u)"
+if [[ "$platform" == "Darwin" ]]; then
+    user_option=""
+fi
+
+docker run  -v $DIR/build:/home/build $user_option $IMG
 
